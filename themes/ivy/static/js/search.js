f01a62d0a98b4e7f495c0e150adc2fc8360057a2
cosine00 <huxin1216@qq.com>
2023-04-07 17:53:19 +0800

    update

--- themes/ivy/static/js/search.js
+++ themes/ivy/static/js/search.js
@@ -1,118 +1,136 @@
-document.addEventListener('DOMContentLoaded', async () => {
-  const searchButton = document.querySelector('#searchBoxButton')
-  const searchInput = document.querySelector('#searchBoxInput')
-  const searchResult = document.querySelector('#searchResult')
-  const searchCount = document.querySelector('#searchCount')
-
-  function parseLocationSearch() {
-    let locationSearch = location.search
-    let result = {}
-    if (locationSearch == '') {
-      return result
-    }
-    locationSearch = locationSearch.substr(1)
-    for (let kv of locationSearch.split('&')) {
-      if (kv == '') {
-        continue
-      }
-      const [k, v] = kv.split('=')
-      result[k] = decodeURI(v)
-    }
-    return result
-  }
-
-  var fuseOptions = {
+var summaryInclude = 180;
+var fuseOptions = {
     shouldSort: true,
     includeMatches: true,
-    threshold: 0.0,
+    includeScore: true,
     tokenize: true,
     location: 0,
     distance: 100,
-    maxPatternLength: 32,
     minMatchCharLength: 1,
-    keys: [{
-        name: "title",
-        weight: 0.8
-      },
-      {
-        name: "summary",
-        weight: 0.5
-      },
-      {
-        name: "tags",
-        weight: 0.3
-      },
-      {
-        name: "date",
-        weight: 0.3
-      },
+    keys: [
+        {name: "title", weight: 0.45},
+        {name: "contents", weight: 0.4},
+        {name: "tags", weight: 0.1},
+        {name: "categories", weight: 0.05}
     ]
-  };
+};
 
-  let fuse = null
+// =============================
+// Search
+// =============================
 
-  async function getFuse() {
-    if (fuse == null) {
-      const resp = await fetch('/index.json', {
-        method: 'get'
-      })
-      const indexData = await resp.json()
-      console.log(indexData.length)
-      fuse = new Fuse(indexData, fuseOptions);
+var inputBox = document.getElementById('search-query');
+if (inputBox !== null) {
+    var searchQuery = param("q");
+    if (searchQuery) {
+        inputBox.value = searchQuery || "";
+        executeSearch(searchQuery, false);
+    } else {
+        document.getElementById('search-results').innerHTML = '<p class="search-results-empty">Please enter a word or phrase above, or see <a href="/tags/">all tags</a>.</p>';
     }
-    return fuse
-  }
+}
 
-  function render(items) {
-    return items.map(item => {
-      item = item.item
-      return `<div class="post on-list">
-                <h1 class="post-title"><a href="${item.permalink}">${item.title}</a></h1>
-                <div class="post-meta"><span class="post-date">${item.date}</span> <span>#${item.tags}</span></div>
-                <div class="post-content">${item.summary}</div>
-              </div>`}).join('')
-  }
+function executeSearch(searchQuery) {
 
-  function updateDOM(html, number) {
-    searchResult.innerHTML = html
-    searchCount.innerHTML = `共查询到 ${number} 篇文章`
-  }
+    show(document.querySelector('.search-loading'));
 
-  async function search() {
-    const searchString = searchInput.value
-    const fuse = await getFuse()
-    const result = fuse.search(searchString)
-    const html = render(result)
-    updateDOM(html, result.length)
-  }
+    fetch('/index.json').then(function (response) {
+        if (response.status !== 200) {
+            console.log('Looks like there was a problem. Status Code: ' + response.status);
+            return;
+        }
+        // Examine the text in the response
+        response.json().then(function (pages) {
+            var fuse = new Fuse(pages, fuseOptions);
+            var result = fuse.search(searchQuery);
+            if (result.length > 0) {
+                populateResults(result);
+            } else {
+                document.getElementById('search-results').innerHTML = '<p class=\"search-results-empty\">No matches found</p>';
+            }
+            hide(document.querySelector('.search-loading'));
+        })
+        .catch(function (err) {
+            console.log('Fetch Error :-S', err);
+        });
+    });
+}
 
-  function doSearch() {
-    const wd = parseLocationSearch()['q'] || ''
-    searchInput.value = wd
-    if (wd) {
-      search()
+function populateResults(results) {
+
+    var searchQuery = document.getElementById("search-query").value;
+    var searchResults = document.getElementById("search-results");
+
+    // pull template from hugo template definition
+    var templateDefinition = document.getElementById("search-result-template").innerHTML;
+
+    results.forEach(function (value, key) {
+
+        var contents = value.item.contents;
+        var snippet = "";
+        var snippetHighlights = [];
+
+        snippetHighlights.push(searchQuery);
+        snippet = contents.substring(0, summaryInclude * 2) + '&hellip;';
+
+        //replace values
+        var tags = ""
+        if (value.item.tags) {
+            value.item.tags.forEach(function (element) {
+                tags = tags + "<a href='/tags/" + element + "'>" + "#" + element + "</a> "
+            });
+        }
+
+        var output = render(templateDefinition, {
+            key: key,
+            title: value.item.title,
+            link: value.item.permalink,
+            tags: tags,
+            categories: value.item.categories,
+            snippet: snippet
+        });
+        searchResults.innerHTML += output;
+
+        snippetHighlights.forEach(function (snipvalue, snipkey) {
+            var instance = new Mark(document.getElementById('summary-' + key));
+            instance.mark(snipvalue);
+        });
+
+    });
+}
+
+function render(templateString, data) {
+    var conditionalMatches, conditionalPattern, copy;
+    conditionalPattern = /\$\{\s*isset ([a-zA-Z]*) \s*\}(.*)\$\{\s*end\s*}/g;
+    //since loop below depends on re.lastInxdex, we use a copy to capture any manipulations whilst inside the loop
+    copy = templateString;
+    while ((conditionalMatches = conditionalPattern.exec(templateString)) !== null) {
+        if (data[conditionalMatches[1]]) {
+            //valid key, remove conditionals, leave contents.
+            copy = copy.replace(conditionalMatches[0], conditionalMatches[2]);
+        } else {
+            //not valid, remove entire section
+            copy = copy.replace(conditionalMatches[0], '');
+        }
     }
-  }
-
-  function goSearch() {
-    searchCount.innerHTML = `查询中…`
-    if (searchInput.value == parseLocationSearch()['q']) {
-      return
+    templateString = copy;
+    //now any conditionals removed we can do simple substitution
+    var key, find, re;
+    for (key in data) {
+        find = '\\$\\{\\s*' + key + '\\s*\\}';
+        re = new RegExp(find, 'g');
+        templateString = templateString.replace(re, data[key]);
     }
-    history.pushState('', '', location.pathname + '?q=' + searchInput.value)
-    searchInput.blur();
-    doSearch()
-  }
+    return templateString;
+}
 
-  searchButton.addEventListener('click', goSearch)
-  searchInput.addEventListener('keyup', (e) => {
-    if (e.key === 'Enter') {
-      goSearch()
-    }
-  })
-
-  doSearch()
-
-  window.addEventListener('popstate', doSearch)
-
-})
+// Helper Functions
+function show(elem) {
+    elem.style.display = 'block';
+}
+function hide(elem) {
+    elem.style.display = 'none';
+}
+function param(name) {
+    return decodeURIComponent((location.search.split(name + '=')[1] || '').split('&')[0]).replace(/\+/g, ' ');
+}
